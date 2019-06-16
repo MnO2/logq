89c2f87abe677b75dce14650306c032542713bbc
Paul Meng <me@paulme.ng>
2019-06-16 14:31:19 +0800

    Build the command line parsing by clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::App;
+use clap::load_yaml;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    if let Some(_matches) = matches.subcommand_matches("cat") {
+        println!("executing command {}", matches.subcommand_name().unwrap());
+    } else if let Some(_) = matches.subcommand_matches("partition") {
+        println!("executing command {}", matches.subcommand_name().unwrap());
+    } else if let Some(_) = matches.subcommand_matches("index") {
+        println!("executing command {}", matches.subcommand_name().unwrap());
+    } else if let Some(_) = matches.subcommand_matches("search") {
+        println!("executing command {}", matches.subcommand_name().unwrap());
+    } else if let Some(_) = matches.subcommand_matches("select") {
+        println!("executing command {}", matches.subcommand_name().unwrap());
+    } else {
+        println!("{}", matches.usage()); 
+    }
 }
