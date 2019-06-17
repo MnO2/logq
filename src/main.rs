fd75b6a558dc5034510cf2604c9b016518e747b1
Paul Meng <me@paulme.ng>
2019-06-17 16:59:52 +0800

    Use positional arguments to parse the arguments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
-use clap::App;
 use clap::load_yaml;
+use clap::App;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let app_m = App::from_yaml(yaml).get_matches();
 
-    if let Some(_matches) = matches.subcommand_matches("cat") {
-        println!("executing command {}", matches.subcommand_name().unwrap());
-    } else if let Some(_) = matches.subcommand_matches("partition") {
-        println!("executing command {}", matches.subcommand_name().unwrap());
-    } else if let Some(_) = matches.subcommand_matches("index") {
-        println!("executing command {}", matches.subcommand_name().unwrap());
-    } else if let Some(_) = matches.subcommand_matches("search") {
-        println!("executing command {}", matches.subcommand_name().unwrap());
-    } else if let Some(_) = matches.subcommand_matches("select") {
-        println!("executing command {}", matches.subcommand_name().unwrap());
-    } else {
-        println!("{}", matches.usage()); 
+    match app_m.subcommand() {
+        ("cat", Some(sub_m)) => {
+            if let Some(files) = sub_m.values_of("files") {
+                let filenames: Vec<&str> = files.collect();
+                println!("{:?}", filenames);
+            } else {
+                app_m.usage();
+            }
+        }
+        ("search", Some(sub_m)) => {
+            if let Some(keyword) = sub_m.value_of("keyword") {
+                println!("{:?}", keyword);
+            } else {
+                app_m.usage();
+            }
+        }
+        ("select", Some(sub_m)) => {
+            if let Some(columns) = sub_m.values_of("columns") {
+                let filenames: Vec<&str> = columns.collect();
+                println!("{:?}", filenames);
+            } else {
+                app_m.usage();
+            }
+        }
+        _ => {
+            app_m.usage();
+        }
     }
 }
