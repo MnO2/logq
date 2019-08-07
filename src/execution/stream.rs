a62e7f746f1cb201096e54b3549323388b8e2ab8
Paul Meng <me@paulme.ng>
2019-08-07 17:47:58 +0800

    Add test_map_stream_with_star and test_map_stream_with_names for unit tests

--- src/execution/stream.rs
+++ src/execution/stream.rs
@@ -1,5 +1,5 @@
 use super::datasource::{Reader, StringRecord};
-use super::types::{Formula, Named, StreamResult};
+use super::types::{Expression, Formula, Named, StreamResult};
 use crate::common;
 use crate::common::types::{Value, VariableName, Variables};
 use std::fs::File;
@@ -24,6 +24,15 @@ impl Record {
 
         variables
     }
+
+    pub(crate) fn to_tuples(&self) -> Vec<(VariableName, Value)> {
+        let mut res = Vec::new();
+        for i in 0..self.field_names.len() {
+            res.push((self.field_names[i].clone(), self.data[i].clone()));
+        }
+
+        res
+    }
 }
 
 pub(crate) trait RecordStream {
@@ -37,33 +46,48 @@ pub(crate) struct MapStream {
     pub(crate) source: Box<dyn RecordStream>,
 }
 
+impl MapStream {
+    pub(crate) fn new(named_list: Vec<Named>, variables: Variables, source: Box<dyn RecordStream>) -> Self {
+        MapStream {
+            named_list,
+            variables,
+            source,
+        }
+    }
+}
+
 impl RecordStream for MapStream {
     fn close(&self) {
         self.source.close();
     }
 
     fn next(&mut self) -> StreamResult<Option<Record>> {
-        let record = self.source.next()?;
-        let variables = self.variables.clone();
+        if let Some(record) = self.source.next()? {
+            let variables = common::types::merge(self.variables.clone(), record.to_variables());
 
-        let mut field_names = Vec::new();
-        let mut data = Vec::new();
-        for named in self.named_list.iter() {
-            match named {
-                Named::Expression(expr, name) => {
-                    field_names.push(name.clone());
-                    let v = expr.expression_value(variables.clone())?;
-                    data.push(v);
-                }
-                Named::Star => {
-                    //TODO: Insert all of the variables in records
-                    unimplemented!();
+            let mut field_names = Vec::new();
+            let mut data = Vec::new();
+            for named in self.named_list.iter() {
+                match named {
+                    Named::Expression(expr, name) => {
+                        field_names.push(name.clone());
+                        let v = expr.expression_value(variables.clone())?;
+                        data.push(v);
+                    }
+                    Named::Star => {
+                        for (k, v) in record.to_tuples().into_iter() {
+                            field_names.push(k);
+                            data.push(v);
+                        }
+                    }
                 }
             }
-        }
 
-        let record = Record::new(field_names, data);
-        Ok(Some(record))
+            let record = Record::new(field_names, data);
+            Ok(Some(record))
+        } else {
+            Ok(None)
+        }
     }
 }
 
@@ -190,6 +214,94 @@ mod tests {
             vec!["host".to_string(), "port".to_string()],
             vec![Value::String("example.com".to_string()), Value::Int(8001)],
         )];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_map_stream_with_star() {
+        let named_list = vec![Named::Star];
+
+        let mut variables: Variables = Variables::default();
+        variables.insert("const".to_string(), Value::String("example.com".to_string()));
+
+        let mut records = VecDeque::new();
+        records.push_back(Record::new(
+            vec!["host".to_string(), "port".to_string()],
+            vec![Value::String("example01.com".to_string()), Value::Int(8000)],
+        ));
+        records.push_back(Record::new(
+            vec!["host".to_string(), "port".to_string()],
+            vec![Value::String("example.com".to_string()), Value::Int(8001)],
+        ));
+        records.push_back(Record::new(
+            vec!["host".to_string(), "port".to_string()],
+            vec![Value::String("example01.com".to_string()), Value::Int(8002)],
+        ));
+        let stream = Box::new(InMemoryStream::new(records));
+
+        let mut filtered_stream = MapStream::new(named_list, variables, stream);
+
+        let mut result = Vec::new();
+        while let Some(n) = filtered_stream.next().unwrap() {
+            result.push(n);
+        }
+
+        let expected = vec![
+            Record::new(
+                vec!["host".to_string(), "port".to_string()],
+                vec![Value::String("example01.com".to_string()), Value::Int(8000)],
+            ),
+            Record::new(
+                vec!["host".to_string(), "port".to_string()],
+                vec![Value::String("example.com".to_string()), Value::Int(8001)],
+            ),
+            Record::new(
+                vec!["host".to_string(), "port".to_string()],
+                vec![Value::String("example01.com".to_string()), Value::Int(8002)],
+            ),
+        ];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_map_stream_with_names() {
+        let named_list = vec![Named::Expression(
+            Expression::Variable("port".to_string()),
+            "port".to_string(),
+        )];
+
+        let mut variables: Variables = Variables::default();
+        variables.insert("const".to_string(), Value::String("example.com".to_string()));
+
+        let mut records = VecDeque::new();
+        records.push_back(Record::new(
+            vec!["host".to_string(), "port".to_string()],
+            vec![Value::String("example01.com".to_string()), Value::Int(8000)],
+        ));
+        records.push_back(Record::new(
+            vec!["host".to_string(), "port".to_string()],
+            vec![Value::String("example.com".to_string()), Value::Int(8001)],
+        ));
+        records.push_back(Record::new(
+            vec!["host".to_string(), "port".to_string()],
+            vec![Value::String("example01.com".to_string()), Value::Int(8002)],
+        ));
+        let stream = Box::new(InMemoryStream::new(records));
+
+        let mut filtered_stream = MapStream::new(named_list, variables, stream);
+
+        let mut result = Vec::new();
+        while let Some(n) = filtered_stream.next().unwrap() {
+            result.push(n);
+        }
+
+        let expected = vec![
+            Record::new(vec!["port".to_string()], vec![Value::Int(8000)]),
+            Record::new(vec!["port".to_string()], vec![Value::Int(8001)]),
+            Record::new(vec!["port".to_string()], vec![Value::Int(8002)]),
+        ];
 
         assert_eq!(expected, result);
     }
