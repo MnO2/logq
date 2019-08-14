6cfd3ac01cb65ef2ccc72d75320de5e4c787c098
Paul Meng <me@paulme.ng>
2019-08-14 14:37:15 +0800

    Adding stub for benchmark

---
+++ benches/logq_benchmark.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+
+fn bench_parse_query() {
+    let _ = 1+1;
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("parse query", |b| b.iter(bench_parse_query));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
