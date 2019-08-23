532c5a6895323f2b7c88435881020c3dac66a27c
Paul Meng <me@paulme.ng>
2019-08-23 13:00:52 +0800

    Add test_run_explain_mode

--- src/app.rs
+++ src/app.rs
@@ -226,7 +226,25 @@ mod tests {
     use tempfile::tempdir;
 
     #[test]
-    fn test_run() {
+    fn test_run_explain_mode() {
+        let query_str = "select * from squid";
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("log_for_test.log");
+        let mut file = File::create(file_path.clone()).unwrap();
+        writeln!(file, r#"1515734740.494      1 [MASKEDIPADDRESS] TCP_DENIED/407 3922 CONNECT d.dropbox.com:443 - HIER_NONE/- text/html"#).unwrap();
+        file.sync_all().unwrap();
+        drop(file);
+
+        let data_source = common::types::DataSource::File(file_path);
+        let result = run(&*query_str, data_source, true, OutputMode::Csv);
+
+        assert_eq!(result, Ok(()));
+
+        dir.close().unwrap();
+    }
+
+    #[test]
+    fn test_run_real_mode() {
         let query_str = "select * from squid";
         let dir = tempdir().unwrap();
         let file_path = dir.path().join("log_for_test.log");
